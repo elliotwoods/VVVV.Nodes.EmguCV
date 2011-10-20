138feafee16097f9ed0fca6c51477e54bdf80aa4
Vadim Smakhtin <algariol@gmail.com>
2011-10-21 00:39:06 +0400

    Merging with my repo. Didn't test for compilation yet )

---
+++ ImageProcessingInstance.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading;
+
+namespace VVVV.Nodes.EmguCV
+{
+	public abstract class ImageProcessingInstance
+	{
+		protected Thread CaptureThread;
+		protected bool RunCaptureThread;
+		protected bool IsRunning;
+
+		protected string Status;
+		
+		public virtual void Close()
+		{
+			RunCaptureThread = false;
+			CaptureThread.Join(100);
+			IsRunning = false;
+		}
+
+		public abstract void Process();
+		
+	}
+}
